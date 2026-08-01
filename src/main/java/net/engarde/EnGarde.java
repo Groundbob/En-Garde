c2fb96fc5d7fcf94ef062dc7a708aaedf0ba1f59
Groundbob <alex24nel@gmail.com>
2026-08-02 09:47:45 +1000

    Added a keybind and key category for Parry and En Garde respectively.

--- src/main/java/net/engarde/EnGarde.java
+++ src/main/java/net/engarde/EnGarde.java
@@ -1,11 +1,18 @@
 package net.engarde;
 
+import com.mojang.blaze3d.platform.InputConstants;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keymapping.v1.KeyMappingHelper;
+import net.minecraft.client.KeyMapping;
+import net.minecraft.client.ToggleKeyMapping;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.Identifier;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 
 public class EnGarde implements ModInitializer {
 	public static final String MOD_ID = "en-garde";
@@ -21,7 +28,29 @@ public class EnGarde implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("Initializing En Garde!");
+
+		KeyMapping.Category CATEGORY = KeyMapping.Category.register(
+				Identifier.fromNamespaceAndPath(EnGarde.MOD_ID, "custom_category")
+		);
+
+		KeyMapping parry = KeyMappingHelper.registerKeyMapping(
+				new ToggleKeyMapping(
+						"key.en-garde.parry",
+						InputConstants.Type.KEYSYM,
+						InputConstants.KEY_R,
+						CATEGORY,
+						() -> true,
+						false
+				));
+
+		ClientTickEvents.END_CLIENT_TICK.register(client -> {
+			while (parry.consumeClick()) {
+				if (client.player != null) {
+					client.player.sendSystemMessage(Component.literal("You have parried!"));
+				}
+			}
+		});
 	}
 
 	public static Identifier id(String path) {
