5c52d4dc73d9b38049a7193b198240ceafdcb38b
Groundbob <alex24nel@gmail.com>
2026-09-08 08:52:49 +1000

    Made it so that the loaded attributes from a datapack display on an item's tooltip

--- src/main/java/net/engarde/mixin/attributes/ItemStackMixin.java
+++ src/main/java/net/engarde/mixin/attributes/ItemStackMixin.java
@@ -9,12 +9,15 @@ import net.minecraft.core.Holder;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.Identifier;
 import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.EquipmentSlotGroup;
 import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.component.ItemAttributeModifiers;
+import org.apache.commons.lang3.function.TriConsumer;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -31,30 +34,7 @@ public class ItemStackMixin {
 
 
         if (slot == EquipmentSlot.MAINHAND && itemConfig != null && itemConfig.combat != null) {
-            if (itemConfig.combat.damage() != null) {
-                AttributeModifier damageModifier = new AttributeModifier(
-                        Identifier.fromNamespaceAndPath(EnGarde.MOD_ID, "config_damage"),
-                        itemConfig.combat.damage() -1,
-                        AttributeModifier.Operation.ADD_VALUE
-                );
-                consumer.accept(Attributes.ATTACK_DAMAGE, damageModifier);
-            }
-            if (itemConfig.combat.speed() != null) {
-                AttributeModifier speedModifier = new AttributeModifier(
-                        Identifier.fromNamespaceAndPath(EnGarde.MOD_ID, "config_speed"),
-                        itemConfig.combat.speed() -4,
-                        AttributeModifier.Operation.ADD_VALUE
-                );
-                consumer.accept(Attributes.ATTACK_SPEED, speedModifier);
-            }
-            if (itemConfig.combat.range() != null) {
-                AttributeModifier rangeModifier = new AttributeModifier(
-                        Identifier.fromNamespaceAndPath(EnGarde.MOD_ID, "config_range"),
-                        itemConfig.combat.range() -3,
-                        AttributeModifier.Operation.ADD_VALUE
-                );
-                consumer.accept(Attributes.ENTITY_INTERACTION_RANGE, rangeModifier);
-            }
+            injectAttributesBiConsumer(itemConfig, consumer);
         }
     }
 
@@ -65,6 +45,87 @@ public class ItemStackMixin {
         ParryItemConfig itemConfig = EnGardeClient.PARRY_ITEM_CONFIGS.get(itemId);
         if (!(itemConfig != null && itemConfig.combat != null)) {
             original.call(instance, slot, consumer);
+        }
+    }
+
+    @Unique
+    private void injectAttributesBiConsumer(ParryItemConfig itemConfig, BiConsumer<Holder<Attribute>, AttributeModifier> consumer) {
+        if (itemConfig.combat.damage() != null) {
+            AttributeModifier damageModifier = new AttributeModifier(
+                    Identifier.fromNamespaceAndPath(EnGarde.MOD_ID, "config_damage"),
+                    itemConfig.combat.damage() -1,
+                    AttributeModifier.Operation.ADD_VALUE
+            );
+            consumer.accept(Attributes.ATTACK_DAMAGE, damageModifier);
+        }
+        if (itemConfig.combat.speed() != null) {
+            AttributeModifier speedModifier = new AttributeModifier(
+                    Identifier.fromNamespaceAndPath(EnGarde.MOD_ID, "config_speed"),
+                    itemConfig.combat.speed() -4,
+                    AttributeModifier.Operation.ADD_VALUE
+            );
+            consumer.accept(Attributes.ATTACK_SPEED, speedModifier);
+        }
+        if (itemConfig.combat.range() != null) {
+            AttributeModifier rangeModifier = new AttributeModifier(
+                    Identifier.fromNamespaceAndPath(EnGarde.MOD_ID, "config_range"),
+                    itemConfig.combat.range() -3,
+                    AttributeModifier.Operation.ADD_VALUE
+            );
+            consumer.accept(Attributes.ENTITY_INTERACTION_RANGE, rangeModifier);
+        }
+    }
+
+    // Dealing with the item tooltips
+    @Inject(method = "forEachModifier(Lnet/minecraft/world/entity/EquipmentSlotGroup;Lorg/apache/commons/lang3/function/TriConsumer;)V", at = @At("HEAD"))
+    private void engarde$overrideTooltip(EquipmentSlotGroup slot, TriConsumer<Holder<Attribute>, AttributeModifier, ItemAttributeModifiers.Display> consumer, CallbackInfo ci) {
+        ItemStack itemStack = (ItemStack) (Object) this;
+        Identifier itemId = BuiltInRegistries.ITEM.getKey(itemStack.getItem());
+        ParryItemConfig itemConfig = EnGardeClient.PARRY_ITEM_CONFIGS.get(itemId);
+
+        if (slot == EquipmentSlotGroup.MAINHAND && itemConfig != null && itemConfig.combat != null) {
+            TriConsumer<Holder<Attribute>, AttributeModifier, ItemAttributeModifiers.Display> triConsumer = (attribute, modifier, _) ->
+                    consumer.accept(attribute, modifier, ItemAttributeModifiers.Display.attributeModifiers());
+            injectAttributesTriConsumer(triConsumer, itemConfig);
+        }
+    }
+
+    @WrapOperation(method = "forEachModifier(Lnet/minecraft/world/entity/EquipmentSlotGroup;Lorg/apache/commons/lang3/function/TriConsumer;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/component/ItemAttributeModifiers;forEach(Lnet/minecraft/world/entity/EquipmentSlotGroup;Lorg/apache/commons/lang3/function/TriConsumer;)V"))
+    private void engarde$cancelOriginalTooltip(ItemAttributeModifiers instance, EquipmentSlotGroup slot, TriConsumer<Holder<Attribute>, AttributeModifier, ItemAttributeModifiers.Display> consumer, Operation<Void> original) {
+        ItemStack itemStack = (ItemStack) (Object) this;
+        Identifier itemId = BuiltInRegistries.ITEM.getKey(itemStack.getItem());
+        ParryItemConfig itemConfig = EnGardeClient.PARRY_ITEM_CONFIGS.get(itemId);
+        if (!(itemConfig != null && itemConfig.combat != null)) {
+            original.call(instance, slot, consumer);
+        }
+    }
+
+    @Unique
+    private void injectAttributesTriConsumer(TriConsumer<Holder<Attribute>, AttributeModifier, ItemAttributeModifiers.Display> consumer, ParryItemConfig itemConfig) {
+
+        ItemAttributeModifiers.Display display = ItemAttributeModifiers.Display.attributeModifiers();
+        if (itemConfig != null && itemConfig.combat != null) {
+            if (itemConfig.combat.damage() != null) {
+                consumer.accept(Attributes.ATTACK_DAMAGE, new AttributeModifier(
+                        Identifier.fromNamespaceAndPath(EnGarde.MOD_ID, "config_damage"),
+                        itemConfig.combat.damage(),
+                        AttributeModifier.Operation.ADD_VALUE
+                ), display);
+            }
+            if (itemConfig.combat.speed() != null) {
+                consumer.accept(Attributes.ATTACK_SPEED, new AttributeModifier(
+                        Identifier.fromNamespaceAndPath(EnGarde.MOD_ID, "config_speed"),
+                        itemConfig.combat.speed(),
+                        AttributeModifier.Operation.ADD_VALUE
+                ), display);
+            }
+            if (itemConfig.combat.range() != null && itemConfig.combat.range() != 3) {
+                consumer.accept(Attributes.ENTITY_INTERACTION_RANGE, new AttributeModifier(
+                        Identifier.fromNamespaceAndPath(EnGarde.MOD_ID, "config_range"),
+                        itemConfig.combat.range() -3,
+                        AttributeModifier.Operation.ADD_VALUE
+                ), display);
+            }
         }
     }
 }
